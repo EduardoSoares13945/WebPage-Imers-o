b7ef89d57cc035b1ef951cbc1847ecec3cfccbd5
Eduardo Soares <eduaardo.sribeiro@gmail.com>
2024-09-06 20:38:23 -0300

    todos os dados aparecem sem muito esforço!

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
-console.log(adados);
-console.log(adados[4].titulo);
+console.log(info);
+console.log(info[2].titulo);
+
+let section = document.getElementById("resultados-pesquisa");
+console.log(section);
+
+for (let dado of info) {
+
+section.innerHTML += `
+    <div class="item-resultado">
+        <h2>
+            <a href="#">${dado.titulo}</a>
+        </h2>
+        <p class="descricao-meta">${dado.descricao}</p>
+        <a href="${dado.link}" target="_blank">Saiba mais</a>
+    </div>
+`;
+
+};
--- data.js
+++ data.js
@@ -1,43 +1,45 @@
-let adados = [speedracer, carros, velocidadefuriosa, titanic, sonicOFilme, superMarioBrosOFilme, harryPotterEPedraFilosofal];
+let info = [
 
-let speedracer = {
-    titulo : "Speed Racer",
-    descricao : "Speed Racer é um filme cheio de adrenalina e velocidade que nos leva para o mundo das corridas automobilísticas. A história acompanha um jovem piloto talentoso que, junto com sua família, enfrenta desafios e conspirações para conquistar a vitória nas pistas mais perigosas do mundo. Com efeitos visuais incríveis e uma trilha sonora eletrizante, o filme é uma aventura emocionante para todas as idades.",
-    link : "https://www.imdb.com/title/tt0811080/"
-};
+    {
+        titulo : "Speed Racer",
+        descricao : "Speed Racer é um filme cheio de adrenalina e velocidade que nos leva para o mundo das corridas automobilísticas. A história acompanha um jovem piloto talentoso que, junto com sua família, enfrenta desafios e conspirações para conquistar a vitória nas pistas mais perigosas do mundo. Com efeitos visuais incríveis e uma trilha sonora eletrizante, o filme é uma aventura emocionante para todas as idades.",
+        link : "https://www.imdb.com/title/tt0811080/"
+    },
 
-let carros = {
-    titulo : "Carros",
-    descricao : "Carros é um filme da Pixar que nos leva para um mundo onde carros têm vida! A história gira em torno do Relâmpago McQueen, um carro de corrida super rápido e um pouco arrogante, que aprende lições importantes sobre amizade e humildade ao se perder em uma pequena cidade. Com personagens divertidos e uma trilha sonora contagiante, o filme é uma aventura emocionante para todas as idades!",
-    link : "https://www.imdb.com/title/tt0317219/"
-};
+    {
+        titulo : "Carros",
+        descricao : "Carros é um filme da Pixar que nos leva para um mundo onde carros têm vida! A história gira em torno do Relâmpago McQueen, um carro de corrida super rápido e um pouco arrogante, que aprende lições importantes sobre amizade e humildade ao se perder em uma pequena cidade. Com personagens divertidos e uma trilha sonora contagiante, o filme é uma aventura emocionante para todas as idades!",
+        link : "https://www.imdb.com/title/tt0317219/"
+    }, 
 
-let velocidadefuriosa = {
-    titulo : "Velozes e Furiosos",
-    descricao : "Velozes e Furiosos foi o filme que deu início a uma das franquias de ação mais populares do cinema! A história se passa no mundo das corridas de rua, onde carros tunados e muita adrenalina são os protagonistas. O filme foi inspirado em um artigo de revista que falava sobre corridas de rua ilegais em Nova York. Cada carro tem uma personalidade única e é parte fundamental da história. A lealdade e a amizade entre os personagens são tão importantes quanto a velocidade.",
-    link : "https://www.imdb.com/title/tt0232500/"
-};
+    {
+        titulo : "Velozes e Furiosos",
+        descricao : "Velozes e Furiosos foi o filme que deu início a uma das franquias de ação mais populares do cinema! A história se passa no mundo das corridas de rua, onde carros tunados e muita adrenalina são os protagonistas. O filme foi inspirado em um artigo de revista que falava sobre corridas de rua ilegais em Nova York. Cada carro tem uma personalidade única e é parte fundamental da história. A lealdade e a amizade entre os personagens são tão importantes quanto a velocidade.",
+        link : "https://www.imdb.com/title/tt0232500/"
+    }, 
 
-let titanic = {
-    titulo : "Titanic",
-    descricao : "O filme Titanic conta a história de Jack e Rose, dois jovens de classes sociais diferentes que se apaixonam a bordo do famoso navio. Enquanto a paixão floresce, uma tragédia se aproxima: o Titanic colide com um iceberg e começa a afundar. O filme mistura romance, aventura e drama, mostrando a luta pela sobrevivência e as consequências de um destino trágico.",
-    link : "https://www.imdb.com/title/tt0120338/"
-};
+    {
+        titulo : "Titanic",
+        descricao : "O filme Titanic conta a história de Jack e Rose, dois jovens de classes sociais diferentes que se apaixonam a bordo do famoso navio. Enquanto a paixão floresce, uma tragédia se aproxima: o Titanic colide com um iceberg e começa a afundar. O filme mistura romance, aventura e drama, mostrando a luta pela sobrevivência e as consequências de um destino trágico.",
+        link : "https://www.imdb.com/title/tt0120338/"
+    }, 
 
-let sonicOFilme = {
-    titulo: "Sonic o Filme",
-    descricao: "Uma aventura de alta velocidade sobre Sonic the Hedgehog, um ouriço azul rápido como um raio que descobre um mundo que ele nunca soube que existia. Quando o Dr. Robotnik, um cientista malvado, encontra Sonic e decide capturá-lo para usar seus poderes para dominar o mundo, Sonic se junta a um policial humano chamado Tom Wachowski para tentar detê-lo.",
-    link: "https://www.imdb.com/title/tt3794354/"
-};
+    {
+        titulo: "Sonic o Filme",
+        descricao: "Uma aventura de alta velocidade sobre Sonic the Hedgehog, um ouriço azul rápido como um raio que descobre um mundo que ele nunca soube que existia. Quando o Dr. Robotnik, um cientista malvado, encontra Sonic e decide capturá-lo para usar seus poderes para dominar o mundo, Sonic se junta a um policial humano chamado Tom Wachowski para tentar detê-lo.",
+        link: "https://www.imdb.com/title/tt3794354/"
+    }, 
 
-let superMarioBrosOFilme = {
-    titulo: "Super Mario Bros. O Filme",
-    descricao: "Mario e Luigi são transportados para o Reino dos Cogumelos, onde devem trabalhar juntos para derrotar o malvado Bowser e salvar a Princesa Peach. Nesta aventura épica, os irmãos encanadores enfrentam desafios incríveis, exploram mundos coloridos e descobrem o verdadeiro significado da amizade.",
-    link: "https://www.imdb.com/title/tt6718170/",
-};
+    {
+        titulo: "Super Mario Bros. O Filme",
+        descricao: "Mario e Luigi são transportados para o Reino dos Cogumelos, onde devem trabalhar juntos para derrotar o malvado Bowser e salvar a Princesa Peach. Nesta aventura épica, os irmãos encanadores enfrentam desafios incríveis, exploram mundos coloridos e descobrem o verdadeiro significado da amizade.",
+        link: "https://www.imdb.com/title/tt6718170/",
+    }, 
 
-let harryPotterEPedraFilosofal = {
-    titulo: "Harry Potter e a Pedra Filosofal",
-    descricao: "Um garoto órfão descobre que é um bruxo e é convidado para estudar em Hogwarts, uma escola de magia. Lá, ele faz novos amigos e embarca em uma aventura épica para impedir que o poderoso Lord Voldemort retorne ao poder.",
-    link: "https://www.imdb.com/title/tt0241527/",
-};
+    {
+        titulo: "Harry Potter e a Pedra Filosofal",
+        descricao: "Um garoto órfão descobre que é um bruxo e é convidado para estudar em Hogwarts, uma escola de magia. Lá, ele faz novos amigos e embarca em uma aventura épica para impedir que o poderoso Lord Voldemort retorne ao poder.",
+        link: "https://www.imdb.com/title/tt0241527/",
+    }
+    
+];
