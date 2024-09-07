3449d9c6baa4425bb5fbb64bfc1b5ee3a1f2e60e
Eduardo Soares <eduaardo.sribeiro@gmail.com>
2024-09-06 21:06:47 -0300

    aula 4 shenanigans

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-console.log(info);
-console.log(info[2].titulo);
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa");
 
-let section = document.getElementById("resultados-pesquisa");
-console.log(section);
+    let resultados = "";
 
-for (let dado of info) {
+    for (let dado of info) {
 
-section.innerHTML += `
-    <div class="item-resultado">
-        <h2>
-            <a href="#">${dado.titulo}</a>
-        </h2>
-        <p class="descricao-meta">${dado.descricao}</p>
-        <a href="${dado.link}" target="_blank">Saiba mais</a>
-    </div>
-`;
+    resultados += `
+        <div class="item-resultado">
+            <h2>
+                <a href="#">${dado.titulo}</a>
+            </h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href="${dado.link}" target="_blank">Saiba mais</a>
+        </div>
+    `;
 
+    };
+
+    section.innerHTML = resultados;
 };
