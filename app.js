932161fdf8035536b1771c5cd4d821ac1197b3a8
Eduardo Soares <135237845+EduardoSoares13945@users.noreply.github.com>
2024-09-09 14:58:19 -0300

    sim

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 function pesquisar() {
     let section = document.getElementById("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    if (campoPesquisa == "") {
+        section.innerHTML = "Não há resultados para a pesquisa. Por favor, verifique se você digitou algo.";
+        return;
+    }
+
+    let campoPesquisa = campoPesquisa.toLowerCase();
+
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
 
     for (let dado of info) {
-
-    resultados += `
-        <div class="item-resultado">
-            <h2>
-                <a href="#">${dado.titulo}</a>
-            </h2>
-            <p class="descricao-meta">${dado.descricao}</p>
-            <a href="${dado.link}" target="_blank">Saiba mais</a>
-        </div>
-    `;
-
-    };
+        if (dado.titulo.includes(campoPesquisa) || dado.descricao.includes(campoPesquisa)) {
+            titulo = dado.titulo.toLowerCase();
+            descricao = dado.descricao.toLowerCase();
+            resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href="${dado.link}" target="_blank">Saiba mais</a>
+            </div>
+            `;
+        }
+    }
 
     section.innerHTML = resultados;
 };
